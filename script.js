e04419e27153a68c81eff74e219ed4527b458535
aanmol18 <anmoly683@gmail.com>
2026-04-24 11:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const sizeDisplay = document.querySelector("#sizeInfo h1");
+
+function updateSize() {
+    sizeDisplay.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
+updateSize();
+
+window.addEventListener("resize", updateSize);
